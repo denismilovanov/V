192b401a132e940441cd4d9c0a287fbb617a6c05
Denis <me@denismilovanov.net>
2015-06-01 14:51:16 +0300

    Юнит-тест на почту

---
+++ tests/EmailTest.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'bootstrap.php';
+
+use \App\Models\Pusher;
+
+class IOSPushTest extends TestCase {
+
+    public function test1() {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Привет от юнит-теста')
+            ->setFrom(array('postmaster@vmeste-app.ru' => 'Postmaster'))
+            ->setTo([env('DEVELOPER_EMAIL')])
+            ->setBody('Привет от юнит-теста');
+
+        $transport = \Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+
+        $mailer = \Swift_Mailer::newInstance($transport);
+
+        $result = $mailer->send($message);
+    }
+
+}
