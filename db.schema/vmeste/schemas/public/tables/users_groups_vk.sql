9e6c940f7f3b991e86dd558e43d9840426acfbef
De. Mi. <me@denismilovanov.net>
2015-06-18 10:31:21 +0300

    Выкинул группы ВК

--- db.schema/vmeste/schemas/public/tables/users_groups_vk.sql
+++ db.schema/vmeste/schemas/public/tables/users_groups_vk.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE users_groups_vk (
     user_id integer NOT NULL REFERENCES users (id),
-    group_id integer NOT NULL REFERENCES groups_vk (id),
+    group_id integer NOT NULL,
     CONSTRAINT users_groups_vk_pkey PRIMARY KEY (user_id, group_id)
 );
 
