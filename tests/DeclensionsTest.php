70175155b1bc8dfeb75302f539f7e342637533d7
De. Mi. <me@denismilovanov.net>
2015-06-23 11:33:47 +0300

    И еще

---
+++ tests/DeclensionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'bootstrap.php';
+
+use \App\Models\Helper;
+
+mb_internal_encoding("UTF-8");
+
+class DeclensionsTest extends TestCase {
+
+    public function test1() {
+        $names = [
+            'Александра' => ['Александрой', 'Aлександры'],
+            'алекса' => ['Алексой', 'Алексы'],
+            'Александр' => ['Александром', 'Aлександра'],
+            'Юлия' => ['Юлией', 'Юлии'],
+            'сергей' => ['Сергеем', 'Сергея'],
+        ];
+
+        foreach ($names as $name => $decl) {
+            $this->assertEquals(Helper::casusInstrumentalis($name, null), $decl[0]);
+            $this->assertEquals(Helper::genitivus($name, null), $decl[1]);
+        }
+    }
+
+}
